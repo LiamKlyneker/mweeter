4bbed20532c83011a05fb3f29b254dde974e1bc2
Liam Klyneker <liamklyneker@gmail.com>
2023-06-21 15:01:48 -0500

    active clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs/server';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/following', '/welcome'],
+  publicRoutes: ['/'],
 });
 
 export const config = {
